487c8c526a8a5da5386cbeeb76b717b487ce9ad8
Your Name <tunlaton11@gmail.com>
2021-09-11 14:14:36 +0700

    new work

---
+++ lab5/3/script.js
@@ -0,0 +1,18 @@
+
+// let requestURL = 'questionAnswerData.json'; 
+//         let request = new XMLHttpRequest(); 
+//         request.onreadystatechange = function () { 
+//             if (request.readyState == 4 && request.status == 200) {
+//                 dataReportStatus(JSON.parse(request.responseText));
+//             } };
+//         request.open("GET", requestURL, true); 
+//         request.send(); 
+
+//         function dataReportStatus(data) { 
+//             document.write(data[0].question);
+//         }
+
+
+alert("Hello")
+
+
